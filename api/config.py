a89f21de9cc137904d549414f328e38d2bb6f738
John Babatola <johnbabatola@gmail.com>
2022-07-20 01:51:29 +0100

    cross site cookie set

--- api/config.py
+++ api/config.py
@@ -8,6 +8,8 @@ class ApplicationConfig:
 
     JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
     JWT_TOKEN_LOCATION=['cookies']
+    JWT_COOKIE_SECURE=eval(os.environ(['JWT_COOKIE_SECURE']))
+    JWT_COOKIE_SAMESITE=eval(os.environ(['JWT_COOKIE_SAMESITE']))
     JWT_COOKIE_CSRF_PROTECT=eval(os.environ['JWT_COOKIE_CSRF_PROTECT'])
     JWT_CSRF_CHECK_FORM=eval(os.environ['JWT_CSRF_CHECK_FORM'])
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=5)
